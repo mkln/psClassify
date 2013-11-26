ed28297f1f09cfca03c1a4fe8efb2c4e52484e63
mkln <peruzzi@gmail.com>
2013-11-26 11:45:45 +0100

    clean

--- psClassify_R.r
+++ psClassify_R.r
@@ -1,3 +1,8 @@
+# a (very) simple classification algorithm that estimates the
+# probability that a name belongs to a person (and not to another entity
+# eg. company or university)
+
+
 library(glmnet)
 library(ROCR)
 library(plyr)
@@ -5,13 +10,13 @@ library(plyr)
 rm(list = ls())
 setwd("/home/desktop/patstat_data/all_code/psClassify")
 
-
+# read data from python
 all_data <- read.table("r_data/r_input.csv", sep="\t", header=TRUE)
 all_data_classify <- subset(all_data[all_data$certain_not_person == 0,], select= -c(name))
 all_data$pr_is_person <- NA
 all_data$is_person_hat <- NA
 
-labeled_data <- all_data_classify[! is.na(all_data_classify$is_person),]
+labeled_data <- all_data_classify[!is.na(all_data_classify$is_person),]
 yt <- labeled_data[,"is_person"]
 y_weight <- labeled_data[, "patent_ct"]
 
@@ -28,11 +33,13 @@ model_formula <- is_person ~ country + patent_ct +
                         
 X <- model.matrix(model_formula, all_data_classify)
 
+# fit a L1-penalized logistic, weight by patent count
 model <- cv.glmnet(X, yt, family = "binomial", weights = y_weight)
 
 xb <- predict(model, X, s=model$lambda.min)
 p_hat <- 1/(1+exp(-xb))
 
+# precision and recall
 precrec <- performance(prediction(p_hat, yt), "prec", "rec")
 
 recall <- as.numeric(precrec@x.values[[1]])
@@ -47,6 +54,9 @@ res_matrix <- as.matrix(cbind(precision, recall, cutoffs))
 subset_recall1 <- res_matrix[res_matrix[,"recall"] == 1,]
 max_precision <- max(subset_recall1[,"precision"])
 print(paste("best precision for top recall =",max_precision))
+
+# in this case, 'best' = 'max precision given recall=1'
+# (in-sample)
 best_cutoff <- subset_recall1[which(subset_recall1[,"precision"] == max_precision),"cutoffs"]
 
 # is_person have been labeled
@@ -54,6 +64,7 @@ best_cutoff <- subset_recall1[which(subset_recall1[,"precision"] == max_precisio
 the_rest <- all_data_classify
 the_rest$is_person <- 0
 
+# new sample
 X_new <- model.matrix(model_formula, the_rest)
 
 new_xb <- predict(model, as.matrix(X_new), s=model$lambda.min)
